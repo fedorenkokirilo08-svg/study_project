584d9d836e912decfa87b9bf60c3d79508bf4459
fedorenkokirilo08-svg <fedorenkokirilo08@gmail.com>
2025-11-13 03:15:26 +0200

    repair my shit

--- Lab#4/05/Lab4.05/Lab4.05/lab4.5.cpp
+++ Lab#4/05/Lab4.05/Lab4.05/lab4.5.cpp
@@ -1,62 +1,40 @@
 ﻿#include <iostream>
 #include <iomanip>
 #include <cmath>
-#include <windows.h>
-#include <cstdlib>  
-#include <ctime>
+#include <time.h>
 using namespace std;
 
-int main() {
+int main()
+{
+    double x, y, R;
+    cout << "R = "; cin >> R;
+    srand((unsigned)time(NULL));
 
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
-    double R;
-    cout << "Значення радіусу = ";
-    cin >> R;
-    double x0 = -2 * R;
-    double x1 = 2 * R;
-    double y0 = -2 * R;
-    double y1 = 2 * R;
-    double dx = 0.5;
-    double dy = 0.5;
-    double x, y;
-    cout << fixed << setprecision(10);
-    for (int i = 1; i <= 10; i++) {
-        cout << "Постріл #" << i << ": Введіть x = ";
-        cin >> x;
-        cout << "Введіть y = ";
-		cin >> y;
-        bool in_shaded = (x >= 0 && y >= 0 && pow(x - R, 2) + pow(y, 2) <= R * R);
-        bool in_unshaded = (x <= 0 && y <= 0 && pow(x + R, 2) + pow(y, 2) <= R * R);
-
-        cout << "Координати: x = " << x << "; y = " << y << "\n";
-        if (in_shaded && !in_unshaded) {
-            cout << "Попадання!\n";
-        }
-        else {
-            cout << "Промах!\n";
-        }
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "x = "; cin >> x;
+        cout << "y = "; cin >> y;
+        if ((x >= 0 && y >= 0 && pow(x - R, 2) + pow(y, 2) <= R * R) &&
+            !(x <= 0 && y <= 0 && pow(x + R, 2) + pow(y, 2) <= R * R))
+            cout << "yes" << endl;
+        else
+            cout << "no" << endl;
     }
-    srand(time(0));
-    cout << "\nАвтоматичні постріли:\n";
-    for (int i = 1; i <= 10; i++) {
-        x = x0 + static_cast<double>(rand()) / RAND_MAX * (x1 - x0);
-        y = y0 + static_cast<double>(rand()) / RAND_MAX * (y1 - y0);
 
-        bool in_shaded = (x >= 0 && y >= 0 && pow(x - R, 2) + pow(y, 2) <= R * R);
-        bool in_unshaded = (x <= 0 && y <= 0 && pow(x + R, 2) + pow(y, 2) <= R * R);
+    cout << endl << fixed;
 
-        cout << "Постріл #" << i << ": x = " << fixed << setprecision(2) << x
-            << "; y = " << y << " -> ";
-        if (in_shaded && !in_unshaded) {
-            cout << "Попадання\n";
-        }
-        else {
-            cout << "Промах\n";
-        }
+    for (int i = 0; i < 10; i++)
+    {
+        x = 4. * rand() / RAND_MAX - 2;
+        y = 4. * rand() / RAND_MAX - 2;
+        if ((x >= 0 && y >= 0 && pow(x - R, 2) + pow(y, 2) <= R * R) &&
+            !(x <= 0 && y <= 0 && pow(x + R, 2) + pow(y, 2) <= R * R))
+            cout << setw(8) << setprecision(4) << x << " "
+            << setw(8) << setprecision(4) << y << " " << "yes" << endl;
+        else
+            cout << setw(8) << setprecision(4) << x << " "
+            << setw(8) << setprecision(4) << y << " " << "no" << endl;
     }
-    return 0; 
 
-    
-
+    return 0;
 }
